9febbbca2cb88d6a6a4e79812d6abcaa1a16430c
Lomeli12 <anthonylomeli11@gmail.com>
2015-03-02 11:25:07 -0800

    Removed some useless localizations and changed version number.

--- src/main/java/net/lomeli/turtlegun/lib/ModLibs.java
+++ src/main/java/net/lomeli/turtlegun/lib/ModLibs.java
@@ -3,20 +3,14 @@ package net.lomeli.turtlegun.lib;
 public class ModLibs {
     public static final String MOD_ID = "turtlegun";
     public static final String MOD_NAME = "Turtle Gun";
-    public static final int MAJOR = 1, MINOR = 1, REVISION = 0;
+    public static final int MAJOR = 2, MINOR = 0, REVISION = 0;
     public static final String VERSION = MAJOR + "." + MINOR + "." + REVISION;
 
     public static final String PACKAGE_PATH = "net.lomeli.turtlegun.";
     public static final String CLIENT_PROXY = PACKAGE_PATH + "client.ClientProxy";
     public static final String PROXY = PACKAGE_PATH + "core.Proxy";
 
-    public static final String MODEL_URL = "https://raw.githubusercontent.com/Lomeli12/TurtleGun/master/src/main/resources/assets/turtlegun/models/ModelGun.tcn";
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/Lomeli12/TurtleGun/master/update.json";
-
-    private static final String UPDATE = "update.turtlegun.";
-    public static final String UPDATE_MESSAGE = UPDATE + "message";
-    public static final String OLD_VERSION = UPDATE + "old";
-    public static final String NEW_VERSION = UPDATE + "new";
 
     public static int GUN_COOLDOWN = 20;
     public static int TURTLE_COUNTDOWN = 7;
